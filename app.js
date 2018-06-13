998b2ee9c7d13ace0e70e826c621447e19702c6e
vinyll <vincent.agnano@scopyleft.fr>
2018-06-13 17:20:59 +0200

    api is listing first 10 actors

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const mongoose = require('mongoose')
 
+const Actor = require('./models/actor')
 
 const app = new Koa()
 const router = new Router()
 
-// mongoose.connect('mongodb://localhost/eac')
+mongoose.connect('mongodb://localhost/eac')
 
-router.get('/actors', ctx => {
-  actors = [
-    {name: "Musée"},
-    {name: "Danse"},
-  ]
+router.get('/actors', async ctx => {
+  const actors = await Actor.find().limit(10)
   ctx.set('Content-Type', 'application/json')
+  debugger
   ctx.body = JSON.stringify(actors)
 })
 
