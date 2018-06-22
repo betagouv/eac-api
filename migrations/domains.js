6cab06b725959ae9efc5c08a16d96027e00b8983
vinyll <vincent.agnano@scopyleft.fr>
2018-06-22 23:46:30 +0200

    renamed migrations and refactored

--- migrations/domains.js
+++
@@ -1,4 +0,0 @@
-db.actors.find({domain: {$exists: true}}).forEach(actor => {
-  actor.domains = actor.domain.length && actor.domain.split(',').map(a => a.trim())
-  db.actors.update({_id: actor._id}, actor)
-})
---
+++ migrations/index.js
@@ -0,0 +1,23 @@
+print('Extracting Domains.')
+db.actors.find({domain: {$exists: true}}).forEach(actor => {
+  actor.domains = actor.domain.length && actor.domain.split(',').map(a => a.trim())
+  delete actor.domain
+  db.actors.update({_id: actor._id}, actor)
+})
+
+print('Extracting "adresse.data.gouv.fr" latLng to geoJSON.')
+db.actors.find({latitude: {$nin: ['', null]}}).forEach(actor => {
+  actor.loc = {type: 'Point', coordinates: [actor.latitude, actor.longitude]}
+  delete actor.latitude
+  delete actor.longitude
+  db.actors.update({_id: actor._id}, actor)
+})
+
+print('Extracting native latLnt to geoJSON.')
+db.actors.find({latLng: {$nin: ['', null]}}).forEach(actor => {
+  if(!actor.latLng.split) return
+  const coordinates = actor.latLng.split(',').map(v => Number(v.trim()))
+  delete actor.latLng
+  actor.loc = {type: 'Point', coordinates: coordinates}
+  db.actors.update({_id: actor._id}, actor)
+})
