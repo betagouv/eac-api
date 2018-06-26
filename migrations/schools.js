ec5b1ab6eacea6ca09ebd6da534822ef31dd2d1c
vinyll <vincent.agnano@scopyleft.fr>
2018-06-26 16:13:42 +0200

    avoiding CursorNotFound error when importing

--- migrations/schools.js
+++ migrations/schools.js
@@ -4,4 +4,4 @@ db.schools.find({latLng: {$exists: false}}).forEach(school => {
   delete school.latitude
   delete school.longitude
   db.schools.update({_id: school._id}, school)
-})
+}).batch_size(5000)
