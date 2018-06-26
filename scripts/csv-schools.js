22220ab6744fe0cf9ae1d38ca65813bf1ffa1cfa
rap2h <raphaelht@gmail.com>
2018-06-26 17:43:01 +0200

    remove useless code

--- scripts/csv-schools.js
+++ scripts/csv-schools.js
@@ -1,13 +1,11 @@
 // Installation:
-// > npm install get-stream csv-parser proj4 iconv-lite request
+// npm install get-stream csv-parser proj4 iconv-lite request
 //
 // Usage:
-// > node csv-schools.js > result.json
-// > mongoimport --uri=mongodb://localhost:27017/eac -c schools2 --jsonArray --file result.json
+// node csv-schools.js > result.json
 const getStream = require('get-stream').array
 const csvParse = require('csv-parser')
 const proj4 = require('proj4')
-const fs = require('fs')
 const iconv = require('iconv-lite')
 const request = require('request')
 
@@ -19,37 +17,34 @@ const unproj = proj4(
 const url = 'https://www.data.gouv.fr/s/resources/adresse-et-geolocalisation-des-etablissements-denseignement-du-premier-et-second-degres/20160526-143453/DEPP-etab-1D2D.csv'
 
 async function parse() {
-  // Sorry for the callback: so 2015, much async. 
-  request(url).pipe(fs.createWriteStream('./input.csv')).on('finish', async () => {
-    const rows = await getStream(
-      fs.createReadStream('./input.csv')
-      .pipe(iconv.decodeStream('ISO-8859-1'))
-      .pipe(iconv.encodeStream('utf8'))
-      .pipe(csvParse({
-        separator: ';'
-      }))
-    )
+  const rows = await getStream(
+    request(url)
+    .pipe(iconv.decodeStream('ISO-8859-1'))
+    .pipe(iconv.encodeStream('utf8'))
+    .pipe(csvParse({
+      separator: ';'
+    }))
+  )
 
-    const normalizedRows = rows.map(r => {
-      const row = { ...r
-      }
-      if (r.coordonnee_x && r.coordonnee_y) {
-        const x = parseFloat(r.coordonnee_x.replace(/,/g, '.'))
-        const y = parseFloat(r.coordonnee_y.replace(/,/g, '.'))
-        return {
-          name: row.appellation_officielle,
-          postalCode: row.code_postal_uai,
-          city: row.localite_acheminement_uai,
-          position: {
-            type: 'Point',
-            coordinates: unproj.forward([x, y])
-          }
+  const normalizedRows = rows.map(r => {
+    const row = { ...r
+    }
+    if (r.coordonnee_x && r.coordonnee_y) {
+      const x = parseFloat(r.coordonnee_x.replace(/,/g, '.'))
+      const y = parseFloat(r.coordonnee_y.replace(/,/g, '.'))
+      return {
+        name: row.appellation_officielle,
+        postalCode: row.code_postal_uai,
+        city: row.localite_acheminement_uai,
+        position: {
+          type: 'Point',
+          coordinates: unproj.forward([x, y])
         }
       }
-    }).filter(x => x)
+    }
+  }).filter(x => x)
 
-    console.log(JSON.stringify(normalizedRows))
-  })
+  console.log(JSON.stringify(normalizedRows))
 }
 
 parse()
