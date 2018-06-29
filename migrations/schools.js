97a6ba92b852c599d7090144371a9f3ed5f59a57
rap2h <raphaelht@gmail.com>
2018-06-29 14:39:33 +0200

    remove migrations (dead code, not in use anymore)

--- migrations/schools.js
+++
@@ -1,7 +0,0 @@
-print('Fixing school documents.')
-db.schools.find({latLng: {$exists: false}}).forEach(school => {
-  school.loc = {type: 'Point', coordinates: [school.latitude, school.longitude]}
-  delete school.latitude
-  delete school.longitude
-  db.schools.update({_id: school._id}, school)
-}).batch_size(5000)
