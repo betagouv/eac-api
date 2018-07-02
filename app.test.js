0bf2b6df652b214b29348e27771709f8243c40e4
rap2h <raphaelht@gmail.com>
2018-07-02 18:24:07 +0200

    circle ci

--- app.test.js
+++ app.test.js
@@ -2,27 +2,32 @@ const app = require('./app')
 const request = require('supertest')
 const mongoose = require('mongoose')
 
-async function waitForIndexes() {
+describe('Test Schools Route', async () => {
+
   const Actor = require('./models/actor')
   const School = require('./models/school')
-  await Actor.ensureIndexes()
-  await School.ensureIndexes()
-}
-waitForIndexes()
 
-describe('Test Schools Route', () => {
-  const School = require('./models/school')
-  School.create({ 
-    name: 'Lycée Liberté',
-    city: 'Paris',
-    loc: { type: 'Point', coordinates: [1, 2] },
-  })
   const schoolsSearchUrl = '/schools/search/Lycée Liberté Paris'
+
+  ;(async () => {
+    await Actor.ensureIndexes()
+    await School.ensureIndexes()
+    School.create({
+      name: 'Lycée Liberté',
+      city: 'Paris',
+      loc: {
+        type: 'Point',
+        coordinates: [1, 2]
+      },
+    })
+  })()
+
   afterAll(() => {
     mongoose.disconnect()
   })
+
   test('respond with json', async () => {
-    return await request(app.callback())
+    return request(app.callback())
       .get(schoolsSearchUrl)
       .set('Accept', 'application/json')
       .expect(200)
@@ -30,6 +35,6 @@ describe('Test Schools Route', () => {
         expect(res.body[0].name).toBe('Lycée Liberté')
         expect(res.body[0].city).toBe('Paris')
       })
+      done()
   })
-  
 })
