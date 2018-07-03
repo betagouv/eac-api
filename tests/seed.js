3c930a9f8e833e3dc2786dcdb43832a1bf85053f
rap2h <raphaelht@gmail.com>
2018-07-03 10:33:16 +0200

    fix indent

--- tests/seed.js
+++ tests/seed.js
@@ -1,17 +1,20 @@
 const MongoClient = require('mongodb').MongoClient
 
 async function seed() {
-    const client = await MongoClient.connect('mongodb://localhost')
-    const db = await client.db('eac')
-    await db.collection('schools').insertOne({
-        name: 'Lycée Liberté',
-        city: 'Paris',
-        loc: {
-            type: 'Point',
-            coordinates: [1, 2]
-        },
-    })
-    await db.collection('schools').createIndex({name: 'text', city: 'text'})
-    client.close()
+  const client = await MongoClient.connect('mongodb://localhost')
+  const db = await client.db('eac')
+  await db.collection('schools').insertOne({
+    name: 'Lycée Liberté',
+    city: 'Paris',
+    loc: {
+      type: 'Point',
+      coordinates: [1, 2]
+    },
+  })
+  await db.collection('schools').createIndex({
+    name: 'text',
+    city: 'text'
+  })
+  client.close()
 }
 seed()
