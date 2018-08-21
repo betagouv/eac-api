be06f72ec45c666d50cfaf85598b438cbb416793
rap2h <raphaelht@gmail.com>
2018-08-21 09:58:04 +0200

    createdAt on actions

--- models/action.js
+++ models/action.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const ActionSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  actorId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Actor' }]
+  actorId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Actor' }],
+  createdAt: { type: Date, default: Date.now }
 }, {
   strict: false
 })
