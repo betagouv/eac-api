5b22c4381ee07aa5e864b8d0fb427153d5b6c8eb
rap2h <raphaelht@gmail.com>
2018-08-24 11:04:17 +0200

    fix createdAt and deletedAt

--- migrations/index.js
+++ migrations/index.js
@@ -1,6 +1,6 @@
 /* globals db */
 
-// Get all actors having actions, then create a document for each and unset actions in actors.
+print("Get all actors having actions, then create a document for each and unset actions in actors.")
 db.actors.find({
   'actions.0': {
     $exists: true
@@ -18,3 +18,43 @@ db.actors.find({
     })
   })
 })
+
+print("Set updatedAt and createdAt...") 
+const origin = new Date('2018-07-01')
+
+print("  ... No date at all") 
+db.actors.find({
+  updatedAt: null,
+  createdAt: null
+}).forEach(actor => {
+  db.actors.update({
+    _id: actor._id
+  }, {
+    updatedAt: origin,
+    createdAt: origin
+  })
+})
+
+print("  ... Only updatedAt, no createdAt")
+db.actors.find({
+  createdAt: null
+}).forEach(actor => {
+  db.actors.update({
+    _id: actor._id
+  }, {
+    createdAt: actor.updatedAt
+  })
+})
+
+print("  ... Only createdAt, no updatedAt")
+db.actors.find({
+  updatedAt: null
+}).forEach(actor => {
+  db.actors.update({
+    _id: actor._id
+  }, {
+    updatedAt: actor.createdAt
+  })
+})
+
+print("Done 🎊")
