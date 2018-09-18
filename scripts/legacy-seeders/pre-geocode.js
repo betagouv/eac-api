eec6fa40a02b629dc314a44a25bc75581c6192b7
rap2h <raphaelht@gmail.com>
2018-09-18 13:44:53 +0200

    tag seeders as legacy before deletion

--- scripts/legacy-seeders/pre-geocode.js
+++ scripts/legacy-seeders/pre-geocode.js
@@ -9,7 +9,7 @@ async function itemsToCsv (items) {
 }
 
 async function parse () {
-  let rows = require(`${__dirname}/../actors.json`)
+  let rows = require(`${__dirname}/../../actors.json`)
   rows = rows.slice(parseInt(process.argv[2]), parseInt(process.argv[2]) + 1000)
   rows = rows.filter(x => !x.loc)
     .filter(x => `${x.address},${x.city},${x.postalCode}`.length < 190)
