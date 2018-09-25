552a170b2031a3b331bfdc582562811e83ffadc5
vinyll <vincent.agnano@scopyleft.fr>
2018-09-25 17:25:45 +0200

    improved department validator accuracy

--- schematypes.js
+++ schematypes.js
@@ -9,7 +9,7 @@ module.exports = {
   Department: {
     type: 'String',
     validate: {
-      validator: (v) => /\d[0-9AB]\d?/.test(v),
+      validator: (v) => /^(?:9[78][12346]|\d{2}|2[AB])$/.test(v),
       message: props => `${props.value} is not a valid department.`
     }
   }
