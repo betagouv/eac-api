ebf60171d58779b5ee51a6d97be358de8030740b
rap2h <raphaelht@gmail.com>
2018-10-01 22:14:22 +0200

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,8 @@ const router = require('koa-router')({
 const User = require('../models/user')
 const { apiRender } = require('../utils')
 
-router.post('/signup', async ctx => {
+router
+  .post('/signup', async ctx => {
     const params = ctx.request.body
     if (!params.email || !params.password || !params.name) {
       return apiRender(ctx, { message: 'Email, password and name required.' }, 400)
@@ -24,6 +25,7 @@ router.post('/signup', async ctx => {
       return apiRender(ctx, { message: e.message }, 400)
     }
   })
+
   .post('/signin', async ctx => {
     const params = ctx.request.body
     const message = 'Invalid email or password.'
@@ -33,7 +35,6 @@ router.post('/signup', async ctx => {
     }
     const match = await user.comparePassword(params.password)
     return match ? apiRender(ctx, { token: 'JWT - TODO', user }) : apiRender(ctx, { message }, 401)
-    
   })
 
 module.exports = router
