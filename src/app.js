1699e4d42319690a3c193a20a3289880548b2225
rap2h <raphaelht@gmail.com>
2018-10-03 11:41:10 +0200

    passport+jwt

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const passport = require("passport")
+require("./passport")(passport)
 const app = express()
 
 const mongoose = require('mongoose')
@@ -9,6 +11,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/eac', { useNewU
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(passport.initialize())
 
 app.use('/actions', require('./routes/actions'))
 app.use('/actors', require('./routes/actors'))
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const User = require('../models/user')
+const configSecret = require('../config').secret
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
 
 router
-  .post('/signup', async (res, req) => {
+  .post('/signup', async (req, res) => {
     const params = req.body
     if (!params.email || !params.password || !params.name) {
       return res.status(400).send({ message: 'Email, password and name required.' })
@@ -23,15 +26,26 @@ router
     }
   })
 
-  .post('/signin', async (res, req) => {
+  .post('/signin', async (req, res) => {
     const params = req.body
+    console.log(params)
+    if (!params.email || !params.password) {
+      return res.status(400).send({ message: 'Email and password required.' })
+    }
     const message = 'Invalid email or password.'
     const user = await User.findOne({ email: params.email })
     if (!user) {
       return res.status(401).send({ message })
     }
-    const match = await user.comparePassword(params.password)
-    return match ? res.send({ token: 'JWT - TODO', user }) : res.status(401).send({ message })
+    if (await user.comparePassword(params.password)) {
+      const token = jwt.sign({ _id: user._id }, configSecret, { expiresIn: '7d' })
+      return res.send({ token: `JWT ${token}`, user })
+    }
+    return res.status(401).send({ message })
+  })
+
+  .get('/user', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.send({ user: req.user })
   })
 
 module.exports = router
